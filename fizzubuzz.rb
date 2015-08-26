a2c456f414cae9e87df790fd9983669ea895b32c
MikeRobertHughes <hughes.mike.r@gmail.com>
2015-08-26 09:57:01 -0500

    Finish Normal Mode

---
+++ fizzubuzz.rb
@@ -0,0 +1,18 @@
+########################## Normal Mode ##########################
+
+num = 1
+i = 100
+
+while i > 0 do
+  if num % 3 == 0 && num % 5 == 0
+    puts "fizzbuzz"
+  elsif num % 5 == 0
+    puts "buzz"
+  elsif num % 3 == 0
+    puts "fizz"
+  else
+    puts "#{num}"
+  end
+  i -= 1
+  num += 1
+end
