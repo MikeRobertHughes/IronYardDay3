5fd302315ba9bec03fde8f3fc87581f26d15d89c
MikeRobertHughes <hughes.mike.r@gmail.com>
2015-08-26 10:32:20 -0500

    Finish Refactor Mode

--- fizzubuzz.rb
+++ fizzubuzz.rb
@@ -1,6 +1,6 @@
 ########################## Normal Mode ##########################
 
-puts "********* Normal Mode *********** \n\n"
+puts "*********** Normal Mode *********** \n\n"
 
 num = 1
 i = 100
@@ -21,7 +21,7 @@ end
 
 ########################## Hard Mode ##########################
 
-puts "********* Hard Mode *********** \n\n"
+puts "*********** Hard Mode *********** \n\n"
 
 def fizzbuzz(i)
   num = 1
@@ -42,3 +42,42 @@ def fizzbuzz(i)
 end
 
 fizzbuzz(50)
+
+########################## Refactored Mode ##########################
+
+puts "*********** Refactored Mode *********** \n\n"
+
+(1..100).each do |num|
+  if (num % 15).zero?
+    puts "fizzbuzz"
+  elsif (num % 5).zero?
+    puts "buzz"
+  elsif (num % 3).zero?
+    puts "fizz"
+  else
+    puts num
+  end
+end
+
+def fizzbuzz(n)
+  return 'fizzbuzz' if (n % 15).zero?
+  return 'buzz'     if (n % 5).zero?
+  return 'fizz'     if (n % 3).zero?
+  n
+end
+
+1.upto(77) do |n|
+  puts fizzbuzz(n)
+end
+
+
+
+
+
+
+
+
+
+
+
+
