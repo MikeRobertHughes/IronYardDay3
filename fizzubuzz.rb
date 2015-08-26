468666201944a98e50f81efb4ab20a56a34cdb66
MikeRobertHughes <hughes.mike.r@gmail.com>
2015-08-26 09:58:17 -0500

    Finish Hard Mode

--- fizzubuzz.rb
+++ fizzubuzz.rb
@@ -1,18 +1,40 @@
 ########################## Normal Mode ##########################
 
-num = 1
-i = 100
+# num = 1
+# i = 100
 
-while i > 0 do
-  if num % 3 == 0 && num % 5 == 0
-    puts "fizzbuzz"
-  elsif num % 5 == 0
-    puts "buzz"
-  elsif num % 3 == 0
-    puts "fizz"
-  else
-    puts "#{num}"
+# while i > 0 do
+#   if num % 3 == 0 && num % 5 == 0
+#     puts "fizzbuzz"
+#   elsif num % 5 == 0
+#     puts "buzz"
+#   elsif num % 3 == 0
+#     puts "fizz"
+#   else
+#     puts "#{num}"
+#   end
+#   i -= 1
+#   num += 1
+# end
+
+########################## Hard Mode ##########################
+
+def fizzbuzz(i)
+  num = 1
+
+  while i > 0 do
+    if num % 3 == 0 && num % 5 == 0
+      puts "fizzbuzz"
+    elsif num % 5 == 0
+      puts "buzz"
+    elsif num % 3 == 0
+      puts "fizz"
+    else
+      puts "#{num}"
+    end
+    i -= 1
+    num += 1
   end
-  i -= 1
-  num += 1
 end
+
+fizzbuzz(50)
